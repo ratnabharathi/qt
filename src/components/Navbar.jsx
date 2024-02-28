925f796af046147cc2bbb0b19dc642c0073b9b17
ratnabharathi <100298110+ratnabharathi@users.noreply.github.com>
2024-02-28 15:16:54 +0530

    third commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,15 +5,21 @@ import Button from './Button/Button';
 import Search from './Search/Search';
 
 const Navbar = () => {
-    return ( < nav className = { styles.navbar } >
+    return ( <
+        nav className = { styles.navbar } >
         <
         div className = { styles.logo } >
         <
         Logo / >
         <
-        /div> <div >  <Search placeholder = "Search a album of your choice" / >
+        /div> <
+        div >
         <
-        /div> <div className = { styles.button } > <Button text = "Give Feedback" / >
+        Search placeholder = "Search a album of your choice"
+        font = "poppins" / >
+        <
+        /div> <div className = { styles.button } > <Button text = "Give Feedback"
+        font = "poppins" / >
         <
         /div> </nav >
     );
