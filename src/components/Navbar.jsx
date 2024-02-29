8b34955d73968459bc10306b37ea5af7c7b11a31
ratnabharathi <100298110+ratnabharathi@users.noreply.github.com>
2024-02-29 11:50:45 +0530

    third

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
+import React from "react";
 import styles from "./Navbar.module.css";
-import Logo from './Logo/Logo';
-import Button from './Button/Button';
-import Search from './Search/Search';
+import Logo from "./Logo/Logo";
+import Button from "./Button/Button";
+import Search from "./Search/Search";
 
 const Navbar = () => {
-    return ( < nav className = { styles.navbar } >
+    return ( <
+        nav className = { styles.navbar } >
         <
         div className = { styles.logo } >
         <
         Logo / >
         <
-        /div>  <div > <Search placeholder = "Search a album of your choice" / >
+        /div>{" "} <
+        div > { " " } <
+        Search placeholder = "Search a album of your choice" / >
         <
-        /div> <div className = { styles.buttonContainer } > <Button text = "Give Feedback"
+        /div>{" "} <
+        div className = { styles.buttonContainer } > { " " } <
+        Button text = "Give Feedback"
         font = "Poppins" / >
         <
-        /div> </nav >
+        /div>{" "} <
+        /nav>
     );
 };
 
