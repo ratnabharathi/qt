89f3d4be89557bd2743fcd83e4d6acd4f6194342
ratnabharathi <100298110+ratnabharathi@users.noreply.github.com>
2024-03-02 11:02:04 +0530

    first

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,27 @@
 import React from "react";
 import Button from "./Button/Button";
 import Search from "./Search/Search";
-import styles from './Navbar.module.css'
-import LogoImage from "../../assets/Logo.png"
+import styles from './Navbar.module.css';
+import LogoImage from './../assets/logoImage.png.png';
 
 
 export default function Navbar() {
-    return ( <
-        >
+    return (
+
         <
         nav className = { styles.navbar } >
         <
-        div className = { styles.logoDiv } > < img src = { LogoImage }
+        div className = { styles.logoDiv } >
+        <
+        img src = { LogoImage }
         alt = "logo"
         width = { 67 }
         /></div >
         <
         Search search = { "Search a song of your choice" }
-        /> <
-        Button children = "Give Feedback" / >
+        /> <Button children = "Give Feedback" / >
         <
-        /nav> <
-        />
+        /nav> 
+
     )
 }
